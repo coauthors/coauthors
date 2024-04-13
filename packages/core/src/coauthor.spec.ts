d78fd1204fba544e26eaabae3b1186e6d43bc410
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-04-13 18:31:38 +0900

    ci(*): add ci workflow

---
+++ packages/core/src/coauthor.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { coauthor } from "./coauthor";
+
+describe("coauthor", () => {
+  it("should make coauthor string with GitHub username & undefined display names", async () => {
+    expect(await coauthor({ user: "manudeli" })).toBe(
+      "Co-authored-by: Jonghyeon Ko <61593290+manudeli@users.noreply.github.com>",
+    );
+  });
+  it("should make coauthor string with GitHub username & defined display name", async () => {
+    expect(await coauthor({ user: "2-NOW", name: "Hyeonjae Lee" })).toBe(
+      "Co-authored-by: Hyeonjae Lee <71202076+2-NOW@users.noreply.github.com>",
+    );
+    expect(await coauthor({ user: "2-NOW", name: "Whale" })).toBe(
+      "Co-authored-by: Whale <71202076+2-NOW@users.noreply.github.com>",
+    );
+  });
+});
--- packages/core/src/coauthor.ts
+++ packages/core/src/coauthor.ts
@@ -5,7 +5,7 @@ import { githubUser } from "./utils/githubUser";
 export const coauthor = (author: z.infer<typeof authorSchema>) =>
   githubUser(authorSchema.parse(author).user).then(
     (githubUser) =>
-      `Co-authored-by: ${author.name ?? githubUser.name} <${githubUser.id}+${
-        author.user
-      }@users.noreply.github.com>` as const,
+      `Co-authored-by: ${author.name ?? githubUser.name ?? author.user} <${
+        githubUser.id
+      }+${author.user}@users.noreply.github.com>` as const,
   );
