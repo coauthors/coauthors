d0cc3a7a0d5c4356b3a1929739744eb9ec4ba3e7
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-04-13 17:32:50 +0900

    chore: update

--- packages/core/src/utils/githubUser.ts
+++ packages/core/src/utils/githubUser.ts
@@ -1,8 +1,7 @@
 import { githubUserSchema } from "../schemas";
 
-export function githubUser(username: string) {
-  return fetch(`https://api.github.com/users/${username}`).then((res) => {
+export const githubUser = (username: string) =>
+  fetch(`https://api.github.com/users/${username}`).then((res) => {
     if (!res.ok) throw new Error(`Failed to fetch user: ${res.statusText}`);
     return res.json().then(githubUserSchema.parse);
   });
-}
