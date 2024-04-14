268267daef6a2d8ef461348d33509acb55fcf781
2-NOW <kj109888@gmail.com>
2024-04-15 04:40:33 +0900

    Update fetch to universalFetch

--- packages/core/src/utils/githubUser.ts
+++ packages/core/src/utils/githubUser.ts
@@ -1,7 +1,8 @@
 import { githubUserSchema } from "../schemas";
+import { universalFetch } from "./universalFetch";
 
 export const githubUser = (username: string) =>
-  fetch(`https://api.github.com/users/${username}`).then((res) => {
+  universalFetch(`https://api.github.com/users/${username}`).then((res) => {
     if (!res.ok) throw new Error(`Failed to fetch user: ${res.statusText}`);
     return res.json().then(githubUserSchema.parse);
   });
