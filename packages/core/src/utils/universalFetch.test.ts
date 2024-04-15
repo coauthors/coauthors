c3c6384b947118b90379fff4f9e0ace4336d9287
2-NOW <kj109888@gmail.com>
2024-04-15 18:15:48 +0900

    Add universalFetch tests more

--- packages/core/src/utils/universalFetch.test.ts
+++ packages/core/src/utils/universalFetch.test.ts
@@ -1,8 +1,9 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
-import { universalFetch } from "./universalFetch";
 
 describe("universalFetch", () => {
   beforeEach(() => {
+    vi.resetModules();
+
     vi.stubGlobal("fetch", undefined);
     vi.stubGlobal("window", undefined);
     vi.stubGlobal("process", { versions: { node: undefined } });
@@ -16,9 +17,26 @@ describe("universalFetch", () => {
     const mockFetch = vi.fn();
     vi.stubGlobal("window", { fetch: mockFetch });
 
-    await universalFetch("https://example.com");
+    const mod = await import("./universalFetch");
+    await mod.universalFetch("https://example.com");
 
     expect(mockFetch).toHaveBeenCalled();
+  });
+
+  it("should not reinitialize fetchFn if it is already set", async () => {
+    const mockFetch = vi.fn();
+    vi.stubGlobal("window", { fetch: mockFetch });
+
+    const mod = await import("./universalFetch");
+    await mod.universalFetch("https://example.com");
+
+    expect(mockFetch).toHaveBeenCalled();
+
+    const mockFetch2 = vi.fn();
+    vi.stubGlobal("window", { fetch: mockFetch2 });
+
+    await mod.universalFetch("https://example.com");
+    expect(mockFetch2).not.toHaveBeenCalled();
   });
 
   it("should use node-fetch in Node.js for versions < 18", async () => {
@@ -27,7 +45,10 @@ describe("universalFetch", () => {
     const mockFetch = vi.fn();
     vi.doMock("node-fetch", async () => ({ default: mockFetch }));
 
-    await universalFetch("https://example.com");
+    const mod = await import("./universalFetch");
+    // wait for node-fetch to be imported
+    await vi.dynamicImportSettled();
+    await mod.universalFetch("https://example.com");
 
     expect(mockFetch).toHaveBeenCalled();
   });
@@ -38,13 +59,32 @@ describe("universalFetch", () => {
     const mockFetch = vi.fn();
     vi.stubGlobal("fetch", mockFetch);
 
-    await universalFetch("https://example.com");
+    const mod = await import("./universalFetch");
+    await mod.universalFetch("https://example.com");
 
     expect(mockFetch).toHaveBeenCalled();
   });
 
+  it("should not use node-fetch in Node.js for versions >= 18", async () => {
+    vi.stubGlobal("process", { versions: { node: "18.0.0" } });
+
+    const mockNodeFetch = vi.fn();
+    const mockFetch = vi.fn();
+    vi.doMock("node-fetch", async () => ({ default: mockNodeFetch }));
+    vi.stubGlobal("fetch", mockFetch);
+
+    const mod = await import("./universalFetch");
+    // wait for node-fetch to be imported
+    await vi.dynamicImportSettled();
+    await mod.universalFetch("https://example.com");
+
+    expect(mockFetch).toHaveBeenCalled();
+    expect(mockNodeFetch).not.toHaveBeenCalled();
+  });
+
   it("throws an error if no environment is found", async () => {
-    await expect(universalFetch("https://example.com")).rejects.toThrow(
+    const mod = await import("./universalFetch");
+    await expect(mod.universalFetch("https://example.com")).rejects.toThrow(
       "No fetch implementation found",
     );
   });
