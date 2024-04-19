d93578599a6f91fdd2c4cdd87635dcc558f09655
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-04-20 05:04:18 +0900

    chore: update

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World hi from coauthors!");
+      vscode.window.showInformationMessage("Hello World from coauthors!");
     },
   );
 
