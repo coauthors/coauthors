359b0017132352907a7d0c325f175cfe699a0d4d
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-04-21 18:08:24 +0900

    chore: update

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -1,28 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { StatusBarItem } from "./statusBarItem";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode" is now active!');
+  console.log("Starting Coauthors ...");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "vscode.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from coauthors!");
-    },
+  context.subscriptions.push(
+    vscode.commands.registerCommand("coauthors.add", () => {
+      vscode.window.showInformationMessage("Add Coauthors!");
+    }),
+    vscode.commands.registerCommand("coauthors.site", () => {
+      vscode.env.openExternal(
+        vscode.Uri.parse("https://coauthors.vercel.app/docs/generator"),
+      );
+    }),
+    new StatusBarItem(),
   );
-
-  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
--- packages/vscode/src/test/extension.test.ts
+++ packages/vscode/src/test/extension.test.ts
@@ -1,9 +1,6 @@
 import * as assert from "node:assert";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
 
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
