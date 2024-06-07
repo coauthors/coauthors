0099a5f6675b6f3eaca30ad94726d75473aab4ed
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-06-08 01:52:46 +0900

    chore: update

--- packages/core/src/coauthor.ts
+++ packages/core/src/coauthor.ts
@@ -1,14 +1,10 @@
 import { type Author, authorSchema } from './schemas'
 import { githubUser } from './utils/githubUser'
-
+const a = 'hi'
 export const coauthor = (author: Author) =>
   githubUser(authorSchema.parse(author).user).then(
     (githubUser) =>
       `Co-authored-by: ${author.name ?? githubUser.name ?? author.user} <${
         githubUser.id
       }+${author.user}@users.noreply.github.com>` as const
-
-
-
-      
   )
