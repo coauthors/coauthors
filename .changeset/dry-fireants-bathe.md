1c8e39709b56c2b0635bd94d203758af54728b31
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-06-10 02:40:10 +0900

    feat(vscode): add Source Control Manager to use Coauthors (#66)
    # [New feature] Button to get Co-author easily on VSCode Source Control
    
    We can directly use Source Control's Coauthors button to get Coauthors's
    string
    
    <!--
        A clear and concise description of what this pr is about.
     -->
    
    ![Jun-10-2024
    02-33-08](https://github.com/coauthors/coauthors/assets/61593290/9a9d7ea9-f37d-464a-bf83-d8f96fc14717)
    
    
    ## PR Checklist
    
    - [x] I did below actions if need
    
    1. I read the [Contributing
    Guide](https://github.com/coauthors/coauthors/blob/main/CONTRIBUTING.md)
    2. I added documents and tests.

---
+++ .changeset/dry-fireants-bathe.md
@@ -0,0 +1,5 @@
+---
+"coauthors-vscode": minor
+---
+
+feat(coauthors-vscode): add Coauthors icon on Source Control Manager
--- packages/coauthors-vscode/src/extension.ts
+++ packages/coauthors-vscode/src/extension.ts
@@ -1,12 +1,54 @@
+import { coauthor } from '@coauthors/core'
 import * as vscode from 'vscode'
+import type * as vscode_git from './@types/vscode.git.d'
 import { StatusBarItem } from './statusBarItem'
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log('Starting Coauthors ...')
+async function getGitApi(): Promise<vscode_git.API | undefined> {
+  try {
+    const extension = vscode.extensions.getExtension<vscode_git.GitExtension>('vscode.git')
+    if (extension !== undefined) {
+      const gitExtension = extension.isActive ? extension.exports : await extension.activate()
+
+      return gitExtension.getAPI(1)
+    }
+  } catch {
+    console.log('Waiting for Git API')
+  }
+
+  return undefined
+}
+
+function assert(condition: boolean, message: string): asserts condition {
+  if (!condition) {
+    throw new Error(message)
+  }
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+  while (!(await getGitApi())) {
+    console.log('Waiting for Git API')
+  }
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('coauthors.add', () => {
-      vscode.window.showInformationMessage('Add Coauthors!')
+    vscode.commands.registerCommand('coauthors.add', async () => {
+      const user = await vscode.window.showInputBox({
+        title: 'Please enter GitHub Username of the co-author',
+        placeHolder: 'GitHub Username',
+      })
+      const repository = (await getGitApi())?.repositories[0]
+      assert(typeof repository !== 'undefined', 'No Git repository found')
+      try {
+        if (user) {
+          const coauthorsString = await coauthor({ user })
+          repository.inputBox.value = `${repository.inputBox.value}
+
+${coauthorsString}`
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          vscode.window.showErrorMessage(`Coauthors: ${error.message}`)
+        }
+      }
     }),
     vscode.commands.registerCommand('coauthors.site', () => {
       vscode.env.openExternal(vscode.Uri.parse('https://coauthors.me/generator'))
