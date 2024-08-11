398ef2b0f6077ee28af1e242c795aebcdcf0f02e
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-08-11 16:59:00 +0900

    chore: add nodemon

--- packages/coauthors-bot/src/index.ts
+++ packages/coauthors-bot/src/index.ts
@@ -1,15 +1,39 @@
 import type { Probot } from 'probot'
 
 export default (app: Probot) => {
+  console.log('Yay, the app was loaded!')
+
   app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({
-      body: 'Thanks for opening this issue!',
+    await context.octokit.issues.createComment({
+      issue_number: 81,
+      body: `**Coauthors bot🤖 is ready to add below people as co-author:**
+| Name | Reasons | Actions |
+| :--- | :----- | :------- |
+| @manudeli | https://github.com/coauthors/coauthors/issues/81#issuecomment-2271790698 | [Click here to add this person as co-author](https://coauthors.me/generator?username=manudeli) |
+| @2-NOW | https://github.com/coauthors/coauthors/issues/81#issuecomment-2271790698 | [Click here to add this person as co-author](https://coauthors.me/generator?username=2-NOW) |
+`,
+      owner: 'coauthors',
+      repo: 'coauthors',
     })
-    await context.octokit.issues.createComment(issueComment)
   })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+  app.on('installation', async (context) => {
+    console.log({ event: 'installation', context })
+    await context.octokit.issues.createComment({
+      issue_number: 81,
+      body: 'Thanks for installing me!',
+      owner: 'coauthors',
+      repo: 'coauthors',
+    })
+  })
+
+  app.onAny((context) => {
+    app.log.info({ event: context.name })
+  })
 }
