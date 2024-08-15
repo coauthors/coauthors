cb0c9bcf57e26b28a31696f9241e7e71f7c1257a
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-08-16 00:07:09 +0900

    chore: update

--- packages/coauthors-bot/api/github/webhooks/index.ts
+++ packages/coauthors-bot/api/github/webhooks/index.ts
@@ -2,6 +2,13 @@ import { createNodeMiddleware, createProbot } from 'probot'
 import { app } from '../../../src/app'
 
 export default createNodeMiddleware(app, {
-  probot: createProbot(),
+  probot: createProbot({
+    env: {
+      APP_ID: process.env.APP_ID,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      PRIVATE_KEY: process.env.PRIVATE_KEY!.replace(/\\n/g, '\n'),
+      WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,
+    },
+  }),
   webhooksPath: '/api/github/webhooks',
 })
--- packages/coauthors-bot/src/index.ts
+++ packages/coauthors-bot/src/index.ts
@@ -1,5 +1,3 @@
-import { createNodeMiddleware, createProbot } from 'probot'
 import { app } from './app'
 
 export default app
-export { app, createNodeMiddleware, createProbot }
